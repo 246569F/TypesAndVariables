72bf0a7428d281c75baf01e7eb4e87f8b3afdf46
246569F <246569@student.uek.krakow.pl>
2025-11-14 18:59:01 +0100

    Update 7.6.py

--- 7.6.py
+++ 7.6.py
@@ -1,4 +1,4 @@
-#program sprawdzający czy samochód może jechać po autostradzie
+#program sprawdzający czy samochód może jechać po autostradzie w sposób prosty
 speed = int(input('Enter vehicle speed: '))
-speed <= 140 and speed >= 40
-print(f'Speed is valid: {bool(speed)}')
+fast = speed <= 140 and speed >= 40
+print(f'Speed is valid: {bool(fast)}')
